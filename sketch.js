7d8aaee17ccd00e2a8cfd20ad723fd2f2df192ee
Aaron Chan <32975128+aychan3089@users.noreply.github.com>
2018-09-10 01:36:46 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,9 +4,13 @@ var count = 0;
 var target;
 var maxforce = 0.2;
 
+var rx = 100;
+var ry = 150;
+var rw = 200;
+var rh = 10;
+
 function setup() {
 	createCanvas(400, 300);
-	rocket = new Rocket();
 	population = new Population();
 	target = createVector(width/2, 50);
 }
@@ -23,6 +27,9 @@ function draw() {
 	}
 
 	ellipse(target.x, target.y, 16, 16);
+
+	fill(255);
+	rect(rx, ry, rw, rh);
 }
 
 function Population() {
@@ -44,7 +51,6 @@ function Population() {
 			newRockets[i] = new Rocket(child);
 		}
 		this.rockets = newRockets;
-
 	}
 
 	this.run = function() {
@@ -75,9 +81,6 @@ function Population() {
 			}
 		}
 	}
-
-
-	
 }
 
 function DNA(genes) {
@@ -91,7 +94,6 @@ function DNA(genes) {
 		}
 	}
 
-	
 	this.crossover = function(partner) {
 		var newgenes = [];
 		var mid = floor(random(this.genes.length))
@@ -116,19 +118,19 @@ function DNA(genes) {
 }
 
 
-
 function Rocket(dna) {
 	this.pos = createVector(width/2, height);
 	this.vel = createVector();
 	this.acc = createVector();
+	this.fitness = 0;
+	this.completed = false;
+	this.crashed = false;
 	if (dna) {
 		this.dna = dna;
 	} else {
 		this.dna = new DNA();
 
 	}
-	this.fitness = 0;
-	this.completed = false;
 
 	this.applyForce = function(force) {
 		this.acc.add(force);
@@ -141,6 +143,10 @@ function Rocket(dna) {
 		if (this.completed) {
 			this.fitness *= 10;
 		}
+
+		if (this.crashed) {
+			this.fitness /= 10;
+		}
 	}
 
 	this.update = function() {
@@ -150,13 +156,26 @@ function Rocket(dna) {
 			this.pos = target.copy();
 		}
 
+		if (this.pos.x > rx && this.pos.x < rx + rw && 
+				this.pos.y > ry && this.pos.y < ry + rh) {
+			this.crashed = true;
+		}
+
+		if (this.pos.x > width || this.pos.x < 0) {
+			this.crashed = true;
+		}
+
+		if (this.pos.y > height || this.pos.y < 0) { //took out top collision 
+			this.crashed = true;	// to make scoring work better 
+		}
+
 		this.applyForce(this.dna.genes[count]);
-		if (!this.completed) {
+		if (!this.completed && !this.crashed) {
 			this.vel.add(this.acc);
 			this.pos.add(this.vel);
 			this.acc.mult(0);
+			this.vel.limit(4);
 		}
-		
 	}
 
 	this.show = function() {
